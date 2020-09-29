41cc03b32a1eacffaa6e921750314d2a138d0945
Mihir1003 <32996071+Mihir1003@users.noreply.github.com>
2020-09-29 23:41:58 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var myList *LinkedList = &LinkedList{val:5,next:nil}
+	// myList.add(5)
+	
+	// myList.add(9)
+	
+	// myList.add(6)
+	
+	// myList.add(7)
+	
+	// myList.delete(0)
+	
+	// myListSorted := myList.sort()
+	// fmt.Println((*myList).extractMin())
+	// myList.display()
+	// myListSorted.display()
+
+	myTree := ItemBinarySearchTree{}
+	myTree.Insert(4,1)
+	myTree.Insert(9,1)
+	myTree.Insert(10,1)
+	myTree.Insert(5,9)
+	fmt.Println(myTree.Max())
+
+}
