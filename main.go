97e76a44a0fe263c44235502cef512243d1bf7df
Mihir1003 <32996071+Mihir1003@users.noreply.github.com>
2020-09-30 00:26:43 +0530

    modules

--- main.go
+++ main.go
@@ -1,29 +1,30 @@
 package main
 
 import "fmt"
+import "github.com/mihir1003/DSSE/Data_Structures"
 
 func main() {
 	// var myList *LinkedList = &LinkedList{val:5,next:nil}
 	// myList.add(5)
-	
+
 	// myList.add(9)
-	
+
 	// myList.add(6)
-	
+
 	// myList.add(7)
-	
+
 	// myList.delete(0)
-	
+
 	// myListSorted := myList.sort()
 	// fmt.Println((*myList).extractMin())
 	// myList.display()
 	// myListSorted.display()
 
-	myTree := ItemBinarySearchTree{}
-	myTree.Insert(4,1)
-	myTree.Insert(9,1)
-	myTree.Insert(10,1)
-	myTree.Insert(5,9)
+	myTree = Data_Structures.ItemBinarySearchTree{}
+	myTree.Insert(4, 1)
+	myTree.Insert(9, 1)
+	myTree.Insert(10, 1)
+	myTree.Insert(5, 9)
 	fmt.Println(myTree.Max())
 
 }
