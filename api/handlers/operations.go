5cea3f0dbfebd1a34447e056a4ff54fdded7556e
Mihir1003 <32996071+Mihir1003@users.noreply.github.com>
2020-10-06 08:26:58 +0530

    kjaghsk

--- api/handlers/operations.go
+++ api/handlers/operations.go
@@ -2,8 +2,11 @@ package handlers
 
 import (
 	"api/data"
-	"github.com/mihir1003/dsse/compete"
+	"github.com/mihir1003/DSSE/dataStructures/BinaryTrees"
+	"github.com/mihir1003/DSSE/dataStructure/Lists"
+
 	"log"
+	"time"
 )
 
 import "net/http"
@@ -53,7 +56,88 @@ func (p *Operations) receiveOperations(rw http.ResponseWriter, r *http.Request) 
 	err := op.FromJSON(r.Body)
 	if err != nil {
 		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}else {
+		rw.WriteHeader(http.StatusOK)
+
+		//_,e:=rw.Write(strs)
+		//if e !=nil {
+		//	http.Error(rw,"Invalid request",http.StatusBadRequest)
+		//}
+
 	}
-	compete.compete
+
+
 
 }
+
+func parse(ops *data.Operations) []string {
+	var list []string
+	for _, element := range *ops {
+		switch element.Name {
+		case "add":
+			list = append(list, "add")
+		case "delete":
+			list = append(list, "delete")
+		case "extractMin":
+			list = append(list, "extractMin")
+		case "extractMax":
+			list=append(list,"extractMax")
+		}
+	}
+	return list
+}
+
+type empty interface {
+
+}
+
+type dataStructure struct {
+	name string
+	time time.Duration
+	structure empty
+
+}
+
+type dataStructures []dataStructure
+
+func compete(ops []string) dataStructures {
+	listDataStructures:=dataStructures{dataStructure{
+		name: "list",
+		time: 0,
+		structure: Lists.LinkedList{
+			Val:  0,
+			Next: nil,
+		},
+	},
+	dataStructure{
+		name: "sortedList",
+		time: 0,
+		structure: Lists.SortedLinkedList{
+		Val:  0,
+		Next: nil,
+	},
+	},
+	dataStructure{
+		name: "binaryTree",
+		time: 0,
+		structure: BinaryTrees.ItemBinarySearchTree{root:nill}
+	},
+	}
+	for _,ds := range listDataStructures {
+		for _,o := ops{
+			switch o {
+			case "add":
+				ds.add(0)
+			case "delete":
+				list = append(list, "delete")
+			case "extractMin":
+				list = append(list, "extractMin")
+			case "extractMax":
+				list=append(list,"extractMax")
+			}
+		}
+	}
+
+
+}
+
